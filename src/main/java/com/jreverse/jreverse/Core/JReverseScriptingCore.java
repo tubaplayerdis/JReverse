071ef8cfc1105164f6c8f8996102b70ea3769bca
Aaron Wilk <aaron.wilk@icloud.com>
2024-04-15 19:36:18 -0500

    Instance Class Modification Stuff

--- src/main/java/com/jreverse/jreverse/Core/JReverseScriptingCore.java
+++ src/main/java/com/jreverse/jreverse/Core/JReverseScriptingCore.java
@@ -1,13 +1,6 @@
 package com.jreverse.jreverse.Core;
 
-import org.python.util.PythonInterpreter;
-
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Objects;
 
 
 // ---------------------------------------------------------------
@@ -23,39 +16,9 @@ public class JReverseScriptingCore {
         return "Development not needed";
     }
 
+    public static native Object[] GetClassInstances(String name);
 
     public static String RunScript(String abpath) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream stream = new PrintStream(baos);
-        try(PythonInterpreter interpreter = new PythonInterpreter()) {
-            PrintStream old = System.out;
-            //Set Stream
-            System.setOut(stream);
-            //Test Stream
-            System.out.println("Execution of Script: "+abpath+"\n\n\n");
-
-            if (Objects.isNull(interpreter)) return "Interpreter Engine Was NULL";
-
-            // Check if the file exists
-            if (!Files.exists(Paths.get(abpath))) {
-                return "Script File Does Not Exist";
-            }
-
-            String scripttext = new String(Files.readAllBytes(Paths.get(abpath)));
-
-            if (scripttext.isEmpty()) return "Script Text Is NULL";
-
-            interpreter.exec(scripttext);
-
-
-            String res = "Output: \n"+ stream.toString();
-
-            //Return System.out
-            System.setOut(old);
-
-            return res;
-        } catch (Exception e){
-            return "PY Interpreter Error: "+e.getMessage();
-        }
+        return "Use of this method is Depreciated!";
     }
 }
