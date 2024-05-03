11a23005b9945d46bea2e0e8da534b7381bb4c67
Aaron Wilk <aaron.wilk@icloud.com>
2024-05-02 23:43:25 -0500

    UI changes

--- src/main/java/com/jreverse/jreverse/startupController.java
+++ src/main/java/com/jreverse/jreverse/startupController.java
@@ -57,7 +57,7 @@ public class startupController {
          * if "fx:controller" is not set in fxml
          * fxmlLoader.setController(NewWindowController);
          */
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 430);
         Stage stage = new Stage();
         Image image = new Image(usePath+"/icon/JReverseIcon.png");
         stage.setResizable(false);
